bc7c7bebaa813ca5c44aad88810b3b5e5270e236
underv4lued <102412063+underv4lued@users.noreply.github.com>
2022-04-03 22:42:07 +0300

    Add files via upload

---
+++ scripts/themes.js
@@ -0,0 +1,28 @@
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "styles/light.css";
+    let darkTheme = "styles/dark.css";
+
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+
+    if(currTheme == lightTheme)
+    {
+   	 currTheme = darkTheme;
+   	 theme = "dark";
+    }
+    else
+    {    
+   	 currTheme = lightTheme;
+   	 theme = "light";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    Save(theme);
+}
